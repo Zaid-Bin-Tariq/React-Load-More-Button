49a150d0e2b597ef9dd174ebfb3d6080e67eba1c
Zaid-Bin-Tariq <150109810+Zaid-Bin-Tariq@users.noreply.github.com>
2024-05-24 14:42:42 +0500

    Created a load-more-date app

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [disableButton, setDisableButton] = useState(false);
+
+  async function fetchProducts() {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+      const result = await response.json();
+
+      if (result && result.products && result.products.length) {
+        setProducts((prevData) => [...prevData, ...result.products]);
+        setLoading(false);
+      }
+
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [count]);
+
+  useEffect(() => {
+    if (products && products.length === 194) setDisableButton(true);
+  }, [products]);
+
+  if (loading) {
+    return <div className="loading">Loading data, please wait!</div>;
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="product-container">
+          {products && products.length
+            ? products.map((item) => (
+                <div className="product" key={item.id}>
+                  <img src={item.thumbnail} alt={item.title} />
+                  <p>{item.title}</p>
+                </div>
+              ))
+            : null}
+        </div>
+        <div className="button-container">
+          <button
+            disabled={disableButton}
+            onClick={() => {
+              setCount(count + 1);
+            }}
+          >
+            Load More Products
+          </button>
+          {disableButton ? <p>You have reached products limit</p> : null}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
